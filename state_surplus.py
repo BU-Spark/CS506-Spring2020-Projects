58851d3cff7beefc0e6e0ef478c635d27ce5888c
taylorhazlett <33666450+taylorhazlett@users.noreply.github.com>
2020-02-24 11:21:06 -0500

    Add files via upload

---
+++ state_surplus.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+
+accepted_codes = ['910','911','912','913','914','915','916','917','918','919','920','921','922',
+				 '923','924','925','926','927','928','929','970','971','972','973','974','975']
+
+#df = pd.read_pickle('mapc.pkl'), pickle used on pc
+df = pd.read_csv('mapc.ma_parcels_metrofuture.csv',dtype=object)
+
+df = df[df['luc_1'].isin(accepted_codes) | df['luc_2'].isin(accepted_codes) | 
+		df['luc_adj_1'].isin(accepted_codes)|df['luc_adj_2'].isin(accepted_codes)]
