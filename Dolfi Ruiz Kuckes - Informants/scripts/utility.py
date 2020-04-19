983d2a71964db1c3ba23de19c22a8dbf08aa4b9f
guthrie-kuckes <guskuckes@gmail.com>
2020-04-19 19:40:47 -0400

    Refractored Daniel's import process into utility.py

--- Dolfi Ruiz Kuckes - Informants/scripts/utility.py
+++ Dolfi Ruiz Kuckes - Informants/scripts/utility.py
@@ -21,3 +21,37 @@ appeals_court_data_as_file = open(newest_appeals_court)
 appeals_court = json.load(appeals_court_data_as_file)
 appeals_old = json.load(open(old_appeals_court))
 
+
+#returns all cases, cleaned and grouped into two categories
+#Daniel's code
+def combine_cases():
+    import pandas as pd
+
+    cases = sjc_old #json.load(j)
+    for case in range(len(cases)):
+        # Need cases, headnotes, and text as strings not lists
+        cases[case]["case"] = " ".join(cases[case]["case"])
+        cases[case]["headnote"] = "\n".join(cases[case]["headnote"])
+        cases[case]["text"] = "\n".join(cases[case]["text"])
+    cases = pd.DataFrame(cases)
+
+
+    appeals = appeals_old
+    for case in range(len(appeals)):
+        # Need cases, headnotes, and text as strings not lists
+        appeals[case]["case"] = " ".join(appeals[case]["case"])
+        appeals[case]["headnote"] = "\n".join(appeals[case]["headnote"])
+        appeals[case]["text"] = "\n".join(appeals[case]["text"])
+    appeals = pd.DataFrame(appeals)
+
+    new_cases = sjc
+    new_cases = pd.DataFrame(new_cases)
+
+    new_appeals = appeals_court
+    new_appeals = pd.DataFrame(new_appeals)
+    
+    # Making two pandas dataframes with all the data, one for sjc cases, one for appeals courts. 
+    all_cases = cases.append(new_cases)
+    all_appeals = appeals.append(new_appeals)
+    return all_cases, all_appeals
+
