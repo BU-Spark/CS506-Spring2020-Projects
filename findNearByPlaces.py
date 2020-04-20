904183d412c3ad74b7afab1ddf46e7c8342d7635
Taylor Hazlett <thazlett@bu.edu>
2020-04-19 21:10:10 -0400

    final transport prox code

--- findNearByPlaces.py
+++ findNearByPlaces.py
@@ -7,22 +7,6 @@ from geopy.exc import GeocoderTimedOut
 import time
 from uszipcode import SearchEngine
 
-# calculate driving distance between 2 points unit in miles
-def calculateDistance(lat1, lng1, lat2, lng2):
-    #url = 'http://router.project-osrm.org/route/v1/driving/'
-    token = 'pk.eyJ1IjoidGF5bG9yaGF6bGV0dCIsImEiOiJjazgxc2o5Y24wdTU0M2V0bDNwYjZybGQzIn0.xwzXtsyAWHodfu86pkK4QQ'
-    url = 'https://api.mapbox.com/directions/v5/mapbox/walking/'
-    pos1 = str(lng1) +',' + str(lat1)
-    pos2 = str(lng2) +',' + str(lat2)
-    locations = pos1 +';'+pos2
-    access = locations+'?access_token='+token
-    response = requests.get(url+access)
-    data = json.loads(response.content)
-    if response.status_code == 200:
-        print(data['routes'][0]['distance'])
-        return data['routes'][0]['distance']*0.000621371 # in miles
-    else:
-        return 0
 
 def convertZip(zipStr):
     """
@@ -42,16 +26,7 @@ def do_geocode(address, locator, loop=0):
         if loop >=5:
             return None
         return do_geocode(address, locator, loop+1)
-    
-    
-def findLatitude_Longitude(street, zipCode, state, country, locator, searchEngine):
-    city = searchEngine.by_zipcode(zipCode).major_city
-    fullAddress= street+', '+ city +', '+ state+', '+ zipCode +', '+country
-    print(fullAddress)
-    location = do_geocode(fullAddress, locator)
-    if not location:
-        return [None,None]
-    return [location.latitude, location.longitude]
+
 
 # =============================================================================
 # read filtered dataframe csv file
@@ -78,35 +53,10 @@ df['addr_zip'] = addressZip
 address = df['addr_str']
 df['addr_str'] = address.astype(str)
 
-locator = Nominatim(user_agent="myGeocoder")
-searchEngine= SearchEngine(simple_zipcode=True)
-
-
-geoLocations = [0 for i in range(df.shape[0])]
-for i in range(0, df.shape[0]):
-    lat = df['latitude'][i]
-    long = df['longitude'][i]
-    # street = df['addr_str'][i]
-    # print(street)
-    # zipCode = df['addr_zip'][i]
-    # print(zipCode)
-    # print('Finding ',i,'th location')
-    # if not zipCode or zipCode == '':
-    #     if df[muni][i] != '':
-    #         town = df[muni][i]
-    #         res = search.by_city_and_state(df[town], "ma")
-    #         zipCode = res[0]
-    #         geoLocations[i] = [lat,long]
-    geoLocations[i] = [lat, long]
-    # else:
-    #     geoLocations[i] = findLatitude_Longitude(street, zipCode, 'MA','United States', locator, searchEngine)
-
 # =============================================================================
 # Google Places API Find nearby places
 # =============================================================================
-#REAL KEY
-#api_key='AIzaSyBzJPJge8tfHAsbR4tDlEglR9XF4DJwIBQ'
-api_key = 'AIzaSyA6RX2rguyDZmChndJsSz4OuVaMDSneQ4o' #You'll need your own API key: https://developers.google.com/places/web-service/get-api-key
+#api_key = #You'll need your own API key: https://developers.google.com/places/web-service/get-api-key
 business_types = ['transit_station']
 
 
@@ -152,42 +102,6 @@ for i in range(len(geoLocations)):
         # get number of nearby bus stops for ith location
         numTransitStops[i] = len(total_results)
 
-# # get number of nearby bus stops within 1/2 mile
-# for i in range(len(geoLocations)):
-#     lat = str(geoLocations[i][0])
-#     long = str(geoLocations[i][1])
-#     print('Finding ',i,'th stops')
-#     if lat != 'None' and long != 'None':
-#         time.sleep(1)
-#         total_results = []
-#         get_nearby_places(lat, long, business_types[0],'')
-#         # get number of nearby bus stops for ith location
-#         numBusStops[i] = len(total_results)
-
-# get number of nearby subway stops within 1/2 mile
-# for i in range(len(geoLocations)):
-#     lat = str(geoLocations[i][0])
-#     long = str(geoLocations[i][1])
-#     print('Finding ',i,'th stops')
-#     if lat != 'None' and long != 'None':
-#         time.sleep(1)
-#         total_results = []
-#         get_nearby_places(lat, long, business_types[1],'')
-#         # get number of nearby bus stops for ith location
-#         numSubwayStops[i] = len(total_results)
-# #
-# # # get number of nearby train stops within 1/2 mile
-# for i in range(len(geoLocations)):
-#     lat = str(geoLocations[i][0])
-#     long = str(geoLocations[i][1])
-#     print('Finding ',i,'th stops')
-#     if lat != 'None' and long != 'None':
-#         time.sleep(1)
-#         total_results = []
-#         get_nearby_places(lat, long, business_types[2],'')
-#         # get number of nearby bus stops for ith location
-#         numTrainStops[i] = len(total_results)
-
 # =============================================================================
 # Google API: find average distance to bus, subway, train stops
 # =============================================================================
@@ -217,70 +131,26 @@ def get_avg_distance(lat, long, business_type, next_page,radius=805):
         return
 
 
-#initialize vars to hold transportation proximity information
-avgDistanceTransitStops = [-1 for i in range(len(geoLocations))]
-
-# get avg dist from nearby transit stops within 1/2 miles
-for i in range(len(geoLocations)):
-    lat = str(geoLocations[i][0])
-    long = str(geoLocations[i][1])
-    print('Finding',i,'th stops')
-    if lat != 'None' and long != 'None':
-        time.sleep(1)
-        avg_distance = []
-        get_avg_distance(lat, long, business_types[0],'')
-        # get avg distance of xxx stops for ith location
-        if len(avg_distance) > 0:
-            avgDistanceTransitStops[i] = sum(avg_distance)/len(avg_distance)
-        else:
-            avgDistanceTransitStops[i] = 0
-    else:
-        avgDistanceTransitStops[i] = -1
-        print(i,'th Location info not available!')
-
-# # get avg dist from nearby bus stops within 1/2 miles
-# for i in range(len(geoLocations)):
-#     lat = str(geoLocations[i][0])
-#     long = str(geoLocations[i][1])
-#     print('Finding',i,'th stops')
-#     if lat != 'None' and long != 'None':
-#         time.sleep(1)
-#         avg_distance = []
-#         get_avg_distance(lat, long, business_types[0],'')
-#         # get avg distance of xxx stops for ith location
-#         if len(avg_distance) > 0:
-#             avgDistanceBusStops[i] = sum(avg_distance)/len(avg_distance)
-#         else:
-#             avgDistanceBusStops[i] = 0
-#     else:
-#         avgDistanceBusStops[i] = -1
-#         print(i,'th Location info not available!')
-
-# # get avg dist from nearby subway stops within 1/2 miles
-# for i in range(len(geoLocations)):
-#     lat = str(geoLocations[i][0])
-#     long = str(geoLocations[i][1])
-#     print('Finding',i,'th stops')
-#     if lat != 'None' and long != 'None':
-#         time.sleep(1)
-#         avg_distance = []
-#         get_avg_distance(lat, long, business_types[1],'')
-#         # get avg distance of xxx stops for ith location
-#         if len(avg_disance) > 0:
-#             avgDistanceSubwayStops[i] = sum(avg_distance)/len(avg_distance)
-#         else:
-#             avgDistanceSubwayStops[i] = 0
-#     else:
-#         avgDistanceSubwayStops[i] = -1
-#         print(i,'th Location info not available!')
-#
-
 # =============================================================================
 # Append avg distance to stations within 1/2 mile on the final dataframe
 # =============================================================================
-df['avgDistanceTransitStops'] = avgDistanceTransitStops
-
 df['numTransitStops'] = numTransitStops
 
 # save final result as csv
 df.to_csv('transport_proximity.csv',index=False)
+
+df = df[df['owner_name'].str.endswith('HOUSING AUTHORITY',na=False)==False]
+print(df.shape)
+
+#Individual Parcels with the most stops
+result_parcels = df.sort_values(by='numTransitStops',ascending = False)
+print(tabulate(result_parcels[['muni','owner_name','longitude','latitude','numTransitStops']].head(),headers='keys', tablefmt='psql'))
+
+
+#Municipalities with the most Stops
+muni_parcels = df[['muni','numTransitStops']].groupby(['muni']).sum()
+muni_parcels = muni_parcels.sort_values(by = 'numTransitStops', ascending = False)
+print(tabulate(muni_parcels.head(),headers='keys', tablefmt='psql'))
+
+
+
