65649e1e1c8595f3dd2ee4b62e0e022e901e666b
guthrie-kuckes <guskuckes@gmail.com>
2020-05-01 13:14:22 -0400

    doc2vec attempt

--- Dolfi Ruiz Kuckes - Informants/scripts/utility.py
+++ Dolfi Ruiz Kuckes - Informants/scripts/utility.py
@@ -24,10 +24,7 @@ appeals_old = json.load(open(old_appeals_court))
 
 #returns all cases, cleaned and grouped into two categories
 #Daniel's code
-<<<<<<< HEAD
 #return all_sjc, all_state_appeals
-=======
->>>>>>> selenium_base
 def combine_cases():
     import pandas as pd
 
